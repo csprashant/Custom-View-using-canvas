418725bac1fddce2a00139b696dbc1e90972c0be
prashant <csprshantphuse@gmail.com>
2021-08-04 17:01:17 +0530

    Functionality Added

---
+++ app/src/main/java/com/example/customviewwithcanvas/view/CustView.java
@@ -0,0 +1,37 @@
+package com.example.customviewwithcanvas.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+public class CustView extends View {
+    public CustView(Context context) {
+        super(context);
+    }
+
+    public CustView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public CustView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(Color.GREEN);
+    }
+}
